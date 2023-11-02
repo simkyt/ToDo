34d2b68b2b725d4d2e24c0fcda1c23e5b8b9ad2e
simkyt <99974435+simkyt@users.noreply.github.com>
2023-11-02 15:38:08 +0200

    Initial Commit

---
+++ ToDo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ToDo
+//
+//  Created by Simonas Kytra on 02/11/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
